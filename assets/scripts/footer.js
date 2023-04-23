3ca04767100530c475561da3edeca5bce5c9e867
Kevin <32113593+repneuable@users.noreply.github.com>
2023-04-22 23:56:25 -0400

    Added footer.js calls to some HTML pages for testing

---
+++ assets/scripts/footer.js
@@ -0,0 +1,11 @@
+document.write('<footer>\
+        <div class="footer-left">\
+            <a href="index.html"><img src="assets/images/logo_small.png" alt="Logo"></a>\
+            <p>&copy; 2023 SocioConnect.</p>\
+        </div>\
+        <div class="footer-right">\
+            <a href="index.html">Home</a>\
+            <a href="about.html">About Us</a>\
+            <a href="features.html">Product Features</a>\
+        </div>\
+    </footer>');
