03f1034c6986b64459e2507ae96d04a2951f4c31
Kevin <32113593+repneuable@users.noreply.github.com>
2023-04-27 12:40:13 -0400

    Add conditional logic to donation amount in Flask route
    /submit-donation route needs to accommodate either pre-selected donation amount or custom

--- app.py
+++ app.py
@@ -7,26 +7,36 @@ import datetime
 app = Flask(__name__)
 
 # Flask route to render homepage
+
+
 @app.route("/")
 def home():
     return render_template('index.html')
 
 # Flask route to render About Us page
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 # Flask route to render Product Features page
+
+
 @app.route("/features")
 def features():
     return render_template('features.html')
 
 # Flask route to render Signup Page
+
+
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
 
 # Flask route to handle Signup form submissions
+
+
 @app.route('/submit-form', methods=['POST'])
 def submit_form():
     fname = request.form['first-name']
@@ -54,12 +64,19 @@ def submit_form():
     return 'Success!'
 
 
-# Flask route to handle Signup form submissions
+# Flask route to handle Donation form payments
 @app.route('/submit-donation', methods=['POST'])
 def submit_donation():
     fname = request.form['first-name']
     lname = request.form['last-name']
-    payment = request.form['payment']
+    email = request.form['donation-email']
+    # payment = request.form['amount']
+    # payment = request.form['selected-amount'] if 'selected-amount' in request.form else request.form['amount']
+    if 'custom' in request.form['selected-amount']:
+        payment = request.form['amount']
+    else:
+        payment = request.form['selected-amount']
+
     date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
  # Store the payment data in the database
@@ -70,18 +87,21 @@ def submit_donation():
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 fname TEXT,
                 lname TEXT,
+                email TEXT,
                 payment INT, 
                 date TEXT
             )
         ''')
-        c.execute('INSERT INTO signups (fname, lname, payment, date) VALUES (?, ?, ?, ?)',
-                  (fname, lname, payment, date))
+        c.execute('INSERT INTO donations (fname, lname, email, payment, date) VALUES (?, ?, ?, ?, ?)',
+                  (fname, lname, email, payment, date))
         conn.commit()
     conn.close()
 
     return 'Thank you for your support!'
 
 # Flask route to provide SQLite data to Chart.js when rendered
+
+
 @app.route('/get-data')
 def get_data():
 
